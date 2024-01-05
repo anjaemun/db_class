e90d09bcad574ba933b383df0012691a8b7a449e
anjaemun <ajm0111@naver.com>
2024-01-05 15:55:44 +0900

    20240105 게시판 간단한 기능

--- db_class2.sql
+++ db_class2.sql
@@ -3,11 +3,13 @@ use db_dbclass;
 drop table if exists member_table;
 create table member_table(
 	id bigint auto_increment,
-    member_email varchar(30) not null,
+    member_email varchar(30) not null unique,
     member_name varchar(20) not null,
     member_password varchar(20) not null,
     constraint pk_member primary key(id)
 );
+desc member_table;
+alter table member_table modify column member_email varchar(30) not null unique;
 drop table if exists category_table;
 create table category_table(
 	id bigint auto_increment,
@@ -21,9 +23,9 @@ create table board_table(
     board_writer varchar(30) not null,
     board_contents varchar(500),
     board_hits int,
-    board_created_at datetime,
-    board_update_at datetime,
-    board_file_attached int,
+    board_created_at datetime default now(),
+    board_update_at datetime on update now(),
+    board_file_attached int default 0,
     member_id bigint,
     category_id bigint,
     constraint pk_board primary key(id),
@@ -37,14 +39,14 @@ create table board_file_table(
     stored_file_name varchar(100),
     board_id bigint,
     constraint pk_board_file primary key(id),
-    constraint fk_board_file_board foreign key(board_id) references board_table(id)
+    constraint fk_board_file_board foreign key(board_id) references board_table(id) on delete cascade
 );
 drop table if exists comment_table;
 create table comment_table(
 	id bigint auto_increment,
     comment_writer varchar(30) not null,
 	comment_contents varchar(200) not null,
-    comment_created_at datetime,
+    comment_created_at datetime default now(),
 	board_id bigint,
     member_id bigint,
     constraint pk_comment primary key(id),
@@ -68,3 +70,23 @@ select * from good_table;
 select * from comment_table;
 select * from category_table;
 
+-- 회원 기능
+-- 1. 회원가입(임의의 회원3명 가입)
+insert into member_table(member_email,member_name,member_password) values('aa@aa','aa','aa');
+insert into member_table(member_email,member_name,member_password) values('bb@bb','bb','bb');
+insert into member_table(member_email,member_name,member_password) values('cc@cc','cc','cc');
+-- 2. 이메일 중복체크 
+select * from member_table where member_email='aa@aa';
+-- 3. 로그인
+select * from member_table where member_email='aa@aa' and member_password='1234';
+-- 4. 전체 회원 목록 조회 
+select * from member_table;
+-- 5. 특정 회원만 조회 
+select * from member_table where id=1;
+-- 6. 내정보 수정하기(6.1, 6.2에 해당하는 쿼리문작성)
+	-- 6.1 회원정보 수정화면 요청(회원정보 수정 페이지를 보여준다고 가정했을 때 필요한 쿼리) 
+    select * from member_table where id=1;
+	-- 6.2 회원정보 수정 처리(비밀번호를 변경한다는 상황)	
+	update member_table set member_password = '1234'  where id=1; 
+-- 7. 회원 삭제 또는 탈퇴 
+delete from member_table where id=3;
